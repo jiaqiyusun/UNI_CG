453d0ac3735a76577ba292460dc2a8bc10b48ee5
danielquintas8 <daniel.quintas.lopes@gmail.com>
2020-10-31 17:51:34 +0000

    physical lights + on/off lightBulb

--- Proj3/js/sceneSubjects/GlobalLight.js
+++ Proj3/js/sceneSubjects/GlobalLight.js
@@ -1,19 +1,39 @@
 class GlobalLight {
 	constructor(scene, x, y, z) {
-		this.light = this.createGlobalLight(x, y, z);
-		scene.add(this.light);
+		this.light = this.createLight();
 
+		this.lightBulb = this.createLightBulb();
+
+		this.globalLight = this.createGlobalLight(this.light, x, y, z);
+		scene.add(this.globalLight)
+		
 		const helper = new THREE.CameraHelper( this.light.shadow.camera );
 		scene.add( helper );
 	}
 
-	createGlobalLight(x, y, z) {
+	createGlobalLight(light, x, y, z) {
+		const globalLight = new THREE.Object3D();
+        const support = new THREE.Mesh(new THREE.CylinderGeometry(12.5, 12.5, 1, 50, 50), new THREE.MeshNormalMaterial());
+        support.position.set(0, 1, 0);
+        globalLight.add(this.lightBulb);
+        globalLight.add(support);
+        globalLight.add(light);
+        globalLight.position.set(x, y, z);
+
+        return globalLight;
+	}
+
+	createLightBulb() {
+		const lightBulb = new THREE.Mesh(new THREE.CylinderGeometry(12, 12, 2, 50, 50), new THREE.MeshBasicMaterial({color: 0xffffff}));
+
+		return lightBulb;
+	}
+
+	createLight() {
 		const light = new THREE.DirectionalLight(0xffffff, 1);
 		light.castShadow = true;
-		light.position.set(x, y, z);
 
 		const d = 12.5;
-
 		light.shadow.camera.left = - d;
 		light.shadow.camera.right = d;
 		light.shadow.camera.top = d;
@@ -27,10 +47,12 @@ class GlobalLight {
 		if (this.light.color.getHexString() == "ffffff") {
 			this.light.color.set(0x000000);
             this.light.castShadow = false;
+            this.lightBulb.material.color.set(0x3F3F3F);
         }
 		else {
 			this.light.color.set(0xffffff);
             this.light.castShadow = true;
+            this.lightBulb.material.color.set(0xffffff);
         }
 	}
 
--- Proj3/js/sceneSubjects/SpotLight.js
+++ Proj3/js/sceneSubjects/SpotLight.js
@@ -6,6 +6,8 @@ class SpotLight {
         scene.add(this.target);
 
         this.light = this.createLight(this.target);
+
+        this.lightBulb = this.createLightBulb();
         
         this.spotLight = this.createSpotLight(this.light, x, y, z);
         scene.add(this.spotLight);
@@ -32,11 +34,18 @@ class SpotLight {
 
         return light;
     }
+
+    createLightBulb() {
+        const lightBulb = new THREE.Mesh(new THREE.SphereGeometry(2, 10, 10), new THREE.MeshBasicMaterial({color: 0xffffff}));
+        
+        return lightBulb;
+    }
     
     createSpotLight(light, x, y, z) {
-        const spotLight = new THREE.Mesh(new THREE.SphereGeometry(2, 10, 10), new THREE.MeshNormalMaterial());
+        const spotLight = new THREE.Object3D();
         const cone = new THREE.Mesh(new THREE.ConeGeometry(2, 5, 20, 20), new THREE.MeshNormalMaterial());
         cone.position.set(0, 3, 0);
+        spotLight.add(this.lightBulb);
         spotLight.add(cone);
         spotLight.add(light);
         spotLight.position.set(x, y, z);
@@ -49,17 +58,19 @@ class SpotLight {
 		if (this.light.color.getHexString() == "ffffff") {
 			this.light.color.set(0x000000);
             this.light.castShadow = false;
+            this.lightBulb.material.color.set(0x3F3F3F);
         }
 		else {
 			this.light.color.set(0xffffff);
             this.light.castShadow = true;
+            this.lightBulb.material.color.set(0xffffff);
         }
 	}
 
 	onKeyDown(key) {
     	switch (key.keyCode) {
 			case 48 + this.id: //id
-				this.toggleLight();
+                this.toggleLight();
 				break;
         }
     }
